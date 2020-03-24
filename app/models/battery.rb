d9f17adcebc30b4c5caaea604e45d05a06ae4d91
agivizvari <agivizvari@gmail.com>
2020-03-24 19:04:34 -0400

    Don't know why not work..

--- app/models/battery.rb
+++ app/models/battery.rb
@@ -2,4 +2,11 @@ class Battery < ApplicationRecord
     belongs_to :building
     has_many :employee
     has_many :column
+
+    # IBM Watson
+    # XXX Batteries are deployed across XXX cities
+    nb_batteries = Battery.all
+    nb_cities = Address.city.all
+    request.body = JSON.dump({
+    "text" => "#{nb_batteries} Batteries are deployed across #{nb_cities} cities"})
 end
