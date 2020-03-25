72ea54e1d0cdb2806b7e377cdb5059d13abfb828
agivizvari <agivizvari@gmail.com>
2020-03-24 21:14:53 -0400

    What a nice tuesday evening

--- app/models/elevator.rb
+++ app/models/elevator.rb
@@ -1,7 +1,8 @@
 class Elevator < ApplicationRecord
-    require 'twilio-ruby'
-
+    
     belongs_to :column
+
+    require 'twilio-ruby'
 
     # Twilio
     validates :elevator_status, presence: true
@@ -26,14 +27,17 @@ class Elevator < ApplicationRecord
 
     # IBM Watson
     # There are currently XXX elevators deployed in the XXX buildings of your XXX customers
-    nb_elevators = Elevator.all
-    nb_buildings = Building.all 
-    nb_customers = Customer.all 
-    request.body = JSON.dump({
-        "text" => "There are currently #{nb_elevators} elevators deployed in the #{nb_buildings} buildings of your #{nb_customers} customers."})
+    # nb_elevators = self.all
+    # nb_buildings = Building.where(entity_type: "Building") 
+    # nb_customers = Customer.all 
+    # request.body = JSON.dump({
+    #     "text" => "There are currently #{nb_elevators} elevators deployed in the #{nb_buildings} buildings of your #{nb_customers} customers."})
 
-    # Currently, XXX elevators are not in Running Status and are being serviced
-    nb_not_active_elevators = Elevator.where(e => e.status != 'Active')
-    request.body = JSON.dump({
-        "text" => "Currently, #{nb_not_active_elevators} elevators are not in Running Status and are being serviced."})
+    # # Currently, XXX elevators are not in Running Status and are being serviced
+    # nb_not_active_elevators = Elevator.where(e => e.status != 'Active')
+    # request.body = JSON.dump({
+    #     "text" => "Currently, #{nb_not_active_elevators} elevators are not in Running Status and are being serviced."})
 end
+# b.each do |building|
+# b = building.entity_id
+# address = Adress.find_by_id(b)
--- app/models/lead.rb
+++ app/models/lead.rb
@@ -2,7 +2,7 @@ class Lead < ApplicationRecord
     
 # IBM Watson
 # You currently have XXX leads in your contact requests
-nb_leads = Lead.all
-request.body = JSON.dump({
-  "text" => "You currently have #{nb_leads} leads in your contact requests"})
+# nb_leads = self.all
+# request.body = JSON.dump({
+#   "text" => "You currently have #{nb_leads} leads in your contact requests"})
 end
